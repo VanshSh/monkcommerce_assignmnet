3cdc16db084b987a8e6e81cc47e202a47a2d9442
VanshSh <vanshsharma9354@gmail.com>
2022-10-06 15:44:52 +0530

    feat: :sparkles: Add Header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React from 'react'
+import Header from './components/Header'
+
 function App() {
     return (
-        <h1 className='text-3xl text-cyan-800 font-bold  underline'>
-            Hello world!
-        </h1>
+        <React.Fragment>
+            <Header />
+        </React.Fragment>
     )
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className='text-[#7E8185] p-5 text-xl font-semibold border-b-2 border-[#7e8185]'>
+            Monk Upsell & Cross-sell
+        </div>
+    )
+}
+
+export default Header
